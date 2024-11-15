e7e42bbcedca158e3296ab9396d975555b549fdc
Nuremusabbir <nuremusabbir3@gmail.com>
2024-11-15 22:04:47 +0530

    expense-project

---
+++ frontend/src/main.jsx
@@ -0,0 +1,56 @@
+
+import ReactDom from 'react-dom/client'
+import App from './App.jsx'
+import './index.css';
+import { Route, RouterProvider,createRoutesFromElements } from 'react-router';
+import { createBrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import store from './redux/store.js';
+
+//privateroute
+import PrivateRoute from './components/PrivateRoute.jsx';
+//Auth
+import Login from './pages/Auth/Login.jsx'
+import Register from './pages/Auth/Register.jsx'
+
+import Profile from './pages/User/Profile.jsx';
+import AdminRoute from './pages/Admin/AdminRoute.jsx';
+import UserList from './pages/Admin/UserList.jsx';
+
+import CategoryList from './pages/Admin/CategoryList';
+
+import ProductList from './pages/Admin/ProductList.jsx';
+import AllProducts from './pages/Admin/AllProducts.jsx';  
+import ProductUpdate from './pages/Admin/ProductUpdate.jsx'
+
+
+//import OrderList from './pages/Admin/OrderList.jsx';
+//import AdminDashboard from "./pages/Admin/AdminDashboard.jsx";
+
+const router=createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<App/>}>
+      <Route path="/login" element={<Login/>}/>
+      <Route path="/register" element={<Register/>}/>
+
+      <Route path="" element={<PrivateRoute/>}>
+      <Route path="/profile" element={<Profile/>}/>
+      </Route>
+
+      {/*admin route*/}
+      <Route path="/admin" element={<AdminRoute />}>
+        <Route path="userlist" element={<UserList />} />
+        <Route path="categorylist" element={<CategoryList />} />
+        <Route path="productlist" element={<ProductList />} />
+        <Route path="allproductslist" element={<AllProducts />} />
+        <Route path="productlist/:pageNumber" element={<ProductList />} />
+        <Route path="product/update/:_id" element={<ProductUpdate />} />
+      </Route>
+    </Route>
+    )
+  )
+ReactDom.createRoot(document.getElementById("root")).render(
+<Provider store={store}>
+  <RouterProvider router={router}/>
+</Provider>
+)
